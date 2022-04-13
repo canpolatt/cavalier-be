82d7845914a8e5c0eaca1b5666fee8f178b3ca27
Armagan Dalkiran <armagandalkran20@gmail.com>
2022-04-13 20:31:28 +0300

    order route bug fixed

--- models/Order.js
+++ models/Order.js
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 
-const OrderSchema = new mongoose.Schema({
-
-    userId:{type:String,required:true},
-    products:[
-        {
-            productId:{
-                type:String,        
-            },
-            quantity:{
-                type:Number,
-                default:1
-            },
-            color:{
-                type:String,
-            },
-            size:{
-                type: String
-            }
-        }
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        color: {
+          type: String,
+        },
+        size: {
+          type: String,
+        },
+        image: { type: String },
+        title: { type: String },
+        description: { type: String },
+        price: { type: String },
+      },
     ],
-    totalPrice:{type:Number,required:true},
-    status:{type:String,default:"pending"},
-    address:{type:Object,required:true}
+    totalPrice: { type: Number, required: true },
+    status: { type: String, default: "pending" },
+    address: { type: Object, required: true },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true})
-
-export default mongoose.model("Order",OrderSchema);
+export default mongoose.model("Order", OrderSchema);
