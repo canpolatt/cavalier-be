13b1bd889c258dec0c1947b363d0ee63cdfd9eb8
Armagan Dalkiran <armagandalkran20@gmail.com>
2022-04-16 13:51:56 +0300

    messages schema updated

--- models/MessageBox.js
+++ models/MessageBox.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const MessageSchema = new mongoose.Schema({
 
     userId:{type:String}, //kullanıcı sisteme kayıtlı ise kaydedilebilir.
-    name:{type:String,required:true},
-    surname:{type:String,required:true},
-    phoneNumber:{type:String,required:true},
-    email:{type:String,required:true},
-    message:{type:String,required:true,maxLength:255}
+    name:{type:String},
+    surname:{type:String},
+    phoneNumber:{type:String},
+    email:{type:String},
+    message:{type:String,maxLength:255}
 
 },{timestamps:true});
 
