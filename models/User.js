e3028e34dc13451f7c20f2bed04b4335147fe211
Armagan Dalkiran <armagandalkran20@gmail.com>
2022-06-08 11:34:55 +0300

    register service updated

--- models/User.js
+++ models/User.js
@@ -7,28 +7,6 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     userType: { type: String, default: "USER" },
-    address: [{ 
-      title:{
-        type: String,
-        required:true
-      },
-      phoneNumber:{
-        type: String,
-        required:true
-      },
-      country:{
-        type: String,
-        required:true
-      },
-      city:{
-        type:String,
-        required:true
-      },
-      fullAdress:{
-        type:String,
-        required:true
-      }
-    }], 
   },
   { timestamps: true }
 );
